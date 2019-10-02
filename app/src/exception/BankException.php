c3ecb1cc3861f3c340fd167bc23b7a92c5c59897
Katie <katie.m.dempsey@gmail.com>
2019-10-02 21:38:31 +1300

    Update BankException.php
    Trying out switch statement for exception messages

--- app/src/exception/BankException.php
+++ app/src/exception/BankException.php
@@ -16,4 +16,26 @@ class BankException extends \Exception
    {
        parent::__construct($message);
    }
+    public function __construct($code = 0)
+    {
+        switch($code) {
+            case 0:
+                $message = 'Failed to load account';
+                break;
+            case 1:
+                $message = 'Invalid Transaction Type';
+                break;
+            case 2:
+                $message = 'Invalid Amount';
+                break;
+            case 3:
+                $message = 'Insufficient Balance';
+                break;
+            default:
+                $message = 'Invalid Data Entered';
+                break;
+        }
+
+        parent::__construct($message, $code);
+    }
 }

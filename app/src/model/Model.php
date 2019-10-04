135cf2d57e91c1657ee14b1c504ec77b388919d6
Quo7e <55905768+Quo7e@users.noreply.github.com>
2019-10-04 13:54:41 +1300

    Update Model.php

--- app/src/model/Model.php
+++ app/src/model/Model.php
@@ -114,10 +114,10 @@ class Model
                                                  (NULL, 'CBishop', 'Chris', 'Bishop', '$password', 'chris@gmail.com', '1111', '20/03/1972'),
                                                  (NULL, 'MLittleLamb', 'Mary','LittleLamb', '$password', 'mary@gmail.com','2222', '01/01/2000');";
 
-        $insertAccount = "INSERT INTO `account` VALUES (NULL,'Savings',10000,1,'01/02/2003'),
-                                                       (NULL,'CreditCard',20,1,'02/02/2003'),
-                                                       (NULL,'Savings',300,2,'02/12/2018'),
-                                                       (NULL,'CreditCard',50,2,'02/12/2018');";
+        $insertAccount = "INSERT INTO `account` VALUES (NULL,'Savings',10000,2,'01/02/2003'),
+                                                       (NULL,'CreditCard',20,2,'02/02/2003'),
+                                                       (NULL,'Savings',300,3,'02/12/2018'),
+                                                       (NULL,'CreditCard',50,3,'02/12/2018');";
 
         $insertTransaction = "INSERT INTO `transaction` VALUES (NULL,'Deposit',20,'25/09/2019',1),
                                                                (NULL,'Withdraw',3,'2/02/2018',2),
